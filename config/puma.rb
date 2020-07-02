eb56bf37f1c057115356f11421a0565dcd3c099e
Caroline <cvrieous@gmail.com>
2020-07-02 10:17:12 -0500

    Change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,8 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']        || 3000
-environment ENV['SINATRA_ENV'] || 'development'
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
