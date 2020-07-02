58bf39a2f4b0bcce37b794d49695c0a136a51cfa
Caroline <cvrieous@gmail.com>
2020-07-02 14:19:30 -0500

    Fix the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,14 @@
 
 require 'json'
 
-card_file = File.read('cards/default-cards-20200702050437.json')
+puts 'Parsing file...'
+card_file = File.read('cards/default-cards.json')
 cards_list = JSON.parse(card_file)
 
+puts 'Creating rows...'
 cards_list.each do |card|
-  image_uri = card[:image_uris][:png] if card[:image_uris]
-  Card.where(scryfall_id: card[:id]).first_or_create(name: card[:name], image_uri: image_uri) if image_uri
+  image_uri = card['image_uris']['png'] if card['image_uris']
+  Card.where(scryfall_id: card['id']).first_or_create(name: card['name'], image_uri: image_uri) if image_uri
 end
+
+puts 'Done'
