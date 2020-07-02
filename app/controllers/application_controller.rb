9792dcc4bb78f3dfc2ce0f38570c5b3b5a00a1f1
Caroline <cvrieous@gmail.com>
2020-07-02 15:20:37 -0500

    Add decks and users controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ require 'sysrandom/securerandom'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, (proc { File.join(root, '../views/') })
     set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
     use Rack::Session::Pool, expire_after: 2_592_000 # seconds
     use Rack::Protection::RemoteToken
