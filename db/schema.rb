487f2ca6f687dff2d2d49afe8530b3ed873e3afc
Caroline <cvrieous@gmail.com>
2020-07-03 18:39:41 -0500

    Add role model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_164142) do
+ActiveRecord::Schema.define(version: 2020_07_03_233719) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2020_07_02_164142) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
