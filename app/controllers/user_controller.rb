a04a16f027b408d34a1f5264b2f1fdfd7cd4cee3
Caroline <cvrieous@gmail.com>
2020-07-04 16:38:15 -0500

    Unscope the card search to fix in prod

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -61,7 +61,7 @@ class UserController < ApplicationController
   end
 
   get '/user/decks/:id/add', auth: ['user'] do
-    @cards = Card.where('name like ?', "%#{params[:card_name]}%")
+    @cards = Card.unscoped.where('name like ?', "%#{params[:card_name]}%").group(:name)
 
     erb :'/user/decks/add'
   end
