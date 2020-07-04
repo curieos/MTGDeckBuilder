409d3c54623bd196cb98c7af901ed1d7f683c981
Caroline <cvrieous@gmail.com>
2020-07-04 12:08:34 -0500

    Fix for prod

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -61,7 +61,7 @@ class UserController < ApplicationController
   end
 
   get '/user/decks/:id/add', auth: ['user'] do
-    @cards = Card.where('name like ?', "%#{params[:card_name]}%").select(:id, :name, :image_uri).group(:name)
+    @cards = Card.where('name like ?', "%#{params[:card_name]}%").group(:name, :id)
 
     erb :'/user/decks/add'
   end
