27b4f3d2fb49aa5d91a116567ddbab97f4cb7eb0
Caroline <cvrieous@gmail.com>
2020-07-05 11:40:21 -0500

    Change the unique validator to be case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   has_secure_password
   has_and_belongs_to_many :roles
   has_many :decks
